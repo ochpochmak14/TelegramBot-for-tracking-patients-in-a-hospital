00a9b9186744f1d02c22533d2a944ba297d45b4d
Emin <makhmudovemin441@gmail.com>
2025-04-13 13:54:31 +0500

    final with db

--- main.py
+++ main.py
@@ -1,226 +1,277 @@
 import telebot
 import sqlite3
 import os
-
 from telebot import types
 from datetime import date
 from dotenv import load_dotenv
-from isvalid_isthat_func import *
 
 load_dotenv()
-
 My_Token = os.getenv('TOKEN')
 bot = telebot.TeleBot(My_Token)
 
-
-cur_weekday = date.today()
-
 today = date.today()
-today1 = str(date.today())
+today1 = str(today)
 dtoday = today.strftime("%d/%m/%Y")
 
+def add_birth_date_column():
+    conn = sqlite3.connect('my_database.db')
+    cur = conn.cursor()
+    try:
+        cur.execute('ALTER TABLE data ADD COLUMN birth_date TEXT')
+        conn.commit()
+    except sqlite3.OperationalError:
+        pass
+    finally:
+        conn.close()
 
+add_birth_date_column()
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    global my_d
     global user_id
     user_id = message.from_user.id
-    
+
     conn = sqlite3.connect('my_database.db')
     cur = conn.cursor()
-    
-    cur.execute('CREATE TABLE IF NOT EXISTS data (tg_id int, lastname text,name text,surname text,date text, date2 text)')
+
+    cur.execute('''
+        CREATE TABLE IF NOT EXISTS data (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            tg_id INTEGER,
+            lastname TEXT,
+            name TEXT,
+            surname TEXT,
+            birth_date TEXT,
+            visit_date TEXT,
+            contact TEXT
+        )
+    ''')
     conn.commit()
-    cur.close()
     conn.close()
-    
-    
-    my_message = f'Здравствуйте, <b>Dr.</b><b>{message.from_user.first_name}</b>'
-    bot.send_message(message.chat.id, my_message, parse_mode='html')
-    
-    markup = types.ReplyKeyboardMarkup()
-    
-    name_list = types.KeyboardButton('Получить список пациентов за сегодня')
-    name_input = types.KeyboardButton('Внести пациента')
-    days_list = types.KeyboardButton('Получить список за каждый день недели')
-    
-    markup.row(name_input, days_list)
-    markup.row(name_list)
-    
-    bot.send_message(message.chat.id,'Держите список функций!', reply_markup=markup)
+
+    if message.text == '/start':
+        my_message = f'Здравствуйте, <b>Dr.{message.from_user.first_name}</b>'
+        bot.send_message(message.chat.id, my_message, parse_mode='html')
+
+    send_main_menu(message)
+
+def send_main_menu(message):
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    markup.row(
+        types.KeyboardButton('Внести пациента'),
+        types.KeyboardButton('Получить список за каждый день недели')
+    )
+    markup.row(
+        types.KeyboardButton('Получить список пациентов за сегодня'),
+        types.KeyboardButton('Удалить пациента по ID')
+    )
+    markup.row(
+        types.KeyboardButton('Экспортировать в .txt'),
+        types.KeyboardButton('Главное меню')
+    )
+    bot.send_message(message.chat.id, 'Держите список функций!', reply_markup=markup)
     bot.register_next_step_handler(message, branches)
 
-bot.message_handler(content_types=['text'])       
+@bot.message_handler(content_types=['text'])
 def branches(message):
     if message.text == 'Внести пациента':
-        markup1 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        
-        btn1 = types.KeyboardButton('Да')
-        btn2 = types.KeyboardButton('Нет')
-        btn3 = types.KeyboardButton('Главное меню')
-        markup1.add(btn1, btn2)
-        markup1.add(btn3)
-        
-        bot.send_message(message.chat.id, 'Введите фамилию: ', reply_markup=markup1)
-            
+        bot.send_message(message.chat.id, 'Введите фамилию:')
         bot.register_next_step_handler(message, get_lastname)
-    
+
     elif message.text == 'Получить список пациентов за сегодня':
-        
-       
         conn = sqlite3.connect('my_database.db')
         cur = conn.cursor()
-        
-        cur.execute(f'SELECT * FROM data WHERE tg_id = {user_id}')
+        cur.execute('SELECT * FROM data WHERE tg_id = ? AND visit_date = ?', (user_id, today1))
         us = cur.fetchall()
+        conn.close()
+
         ans = ""
         for i in us:
-            if i[5] == today1:
-                ans += f'ФИО: {i[1]}, {i[2]}, {i[3]}, Дата рождения - {i[4]}\n'
-       
-        cur.close()
-        conn.close()
-        
-        
+            ans += f'ID: {i[0]} | ФИО: {i[2]} {i[3]} {i[4]} | Дата рождения - {i[5]} | Контакт - {i[7]}\n'
+
+        if not ans:
+            ans = 'Сегодня не зарегистрировано пациентов.'
+
         bot.send_message(message.chat.id, ans)
-        start(message)
-        
-    
+        send_main_menu(message)
+
     elif message.text == 'Получить список за каждый день недели':
-        
-         conn = sqlite3.connect('my_database.db')
-         cur = conn.cursor()
-        
-         cur.execute(f'SELECT * FROM data WHERE tg_id = {user_id}')
-         us = cur.fetchall()
-         ans = ""
-         for i in us:
-             
-             ans += f'ФИО: {i[1]}, {i[2]}, {i[3]}, Дата рождения - {i[4]}\n, День посещения - {i[5]}\n'
-        
-         cur.close()
-         conn.close()
-        
-        
-        #  bot.register_next_step_handler(message,start)
-         bot.send_message(message.chat.id, ans)
-         start(message)
-    
+        conn = sqlite3.connect('my_database.db')
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM data WHERE tg_id = ?', (user_id,))
+        us = cur.fetchall()
+        conn.close()
+
+        ans = ""
+        for i in us:
+            if len(i) >= 7:
+                ans += f'ID: {i[0]} | ФИО: {i[2]} {i[3]} {i[4]} | Дата рождения - {i[5]} | День посещения - {i[6]} | Контакт - {i[7]}\n'
+
+        if not ans:
+            ans = 'Нет данных.'
+
+        bot.send_message(message.chat.id, ans)
+        send_main_menu(message)
+
+    elif message.text == 'Удалить пациента по ID':
+        bot.send_message(message.chat.id, 'Введите ID пациента для удаления:')
+        bot.register_next_step_handler(message, delete_by_id)
+
+    elif message.text == 'Главное меню':
+        send_main_menu(message)
+
+    elif message.text == 'Экспортировать в .txt':
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton('Экспортировать за сегодня'), types.KeyboardButton('Экспортировать за весь период'))
+        bot.send_message(message.chat.id, 'Выберите период для экспорта:', reply_markup=markup)
+        bot.register_next_step_handler(message, export_choice)
+
     else:
-        bot.send_message(message.chat.id, 'Выберите одну из опций!༼ つ ◕_◕ ༽つ')
+        bot.send_message(message.chat.id, 'Выберите одну из опций!')
         bot.register_next_step_handler(message, branches)
 
+def export_choice(message):
+    if message.text == 'Экспортировать за сегодня':
+        export_to_txt(message, today1)
+
+    elif message.text == 'Экспортировать за весь период':
+        export_to_txt(message)
+
+    else:
+        bot.send_message(message.chat.id, 'Пожалуйста, выберите один из вариантов!')
+        bot.register_next_step_handler(message, export_choice)
+
+def export_to_txt(message, date_filter=None):
+    conn = sqlite3.connect('my_database.db')
+    cur = conn.cursor()
+
+    if date_filter:
+        cur.execute('SELECT * FROM data WHERE tg_id = ? AND visit_date = ?', (user_id, date_filter))
+    else:
+        cur.execute('SELECT * FROM data WHERE tg_id = ?', (user_id,))
+    
+    patients = cur.fetchall()
+    conn.close()
+
+    file_content = "Список пациентов:\n"
+    for patient in patients:
+        file_content += f'ID: {patient[0]} | ФИО: {patient[2]} {patient[3]} {patient[4]} | Дата рождения - {patient[5]} | Дата посещения - {patient[6]} | Контакт - {patient[7]}\n'
+
+    file_name = f"patients_{today1}.txt" if date_filter else "patients_all_time.txt"
+    with open(file_name, 'w', encoding='utf-8') as f:
+        f.write(file_content)
+
+    with open(file_name, 'rb') as f:
+        bot.send_document(message.chat.id, f)
+
+    bot.send_message(message.chat.id, f"Список пациентов экспортирован в файл: {file_name}")
+    send_main_menu(message)
 
 def get_lastname(message):
     global lastname
-    
     if message.text == 'Главное меню':
-            start(message)
-            
-    elif(is_validate_string(message.text)):
+        start(message)
+    elif message.text.isalpha():
         lastname = message.text
-        bot.send_message(message.chat.id, 'Введите Имя: ')
+        bot.send_message(message.chat.id, 'Введите имя:')
         bot.register_next_step_handler(message, get_name)
     else:
-        bot.send_message(message.chat.id, 'Введите фамилию: ')
+        bot.send_message(message.chat.id, 'Введите фамилию:')
         bot.register_next_step_handler(message, get_lastname)
-
 
 def get_name(message):
     global name
     if message.text == 'Главное меню':
-            start(message)
-            
-    elif(is_validate_string(message.text)):
+        start(message)
+    elif message.text.isalpha():
         name = message.text
-        bot.send_message(message.chat.id, 'Введите Отчество: ')
+        bot.send_message(message.chat.id, 'Введите отчество:')
         bot.register_next_step_handler(message, get_surname)
     else:
-    
-        bot.send_message(message.chat.id, 'Введите Имя: ')
+        bot.send_message(message.chat.id, 'Введите имя:')
         bot.register_next_step_handler(message, get_name)
-    
-    
+
 def get_surname(message):
     global surname
     if message.text == 'Главное меню':
-            start(message)
-            
-    elif(is_validate_string(message.text)):
+        start(message)
+    elif message.text.isalpha():
         surname = message.text
-        markup2 = types.ReplyKeyboardMarkup()
-    
-        yes_k = types.KeyboardButton('Да')
-        no_k = types.KeyboardButton('Нет')
-    
-        markup2.add(yes_k)
-        markup2.add(no_k)
-
-        
-        bot.send_message(message.chat.id, f'Фамилия - {lastname}, Имя - {name}, Отчество - {surname} || <b>Все верно?</b> ',reply_markup=markup2, parse_mode='html')
+        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+        markup.add(types.KeyboardButton('Да'), types.KeyboardButton('Нет'))
+        bot.send_message(
+            message.chat.id,
+            f'Фамилия - {lastname}, Имя - {name}, Отчество - {surname}. Всё верно?',
+            reply_markup=markup
+        )
         bot.register_next_step_handler(message, callback_one)
     else:
-        bot.send_message(message.chat.id, 'Введите Отчество: ')
-        bot.register_next_step_handler(message, get_surname)    
-
-
-def get_date(message):
-    global birth_date
-    if message.text == 'Главное меню':
-            start(message)
-            
-    elif(is_validate_date(message.text)):
-        birth_date = message.text
-        
-        markup001 = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        
-        btn1 = types.KeyboardButton('Да')
-        btn2 = types.KeyboardButton('Нет')
-        
-        bot.send_message(message.chat.id, f'Дата рождения - {birth_date}? <b>Все верно?</b>', reply_markup=markup001, parse_mode='html')
-        bot.register_next_step_handler(message, callback_two)
-    else:
-        bot.send_message(message.chat.id, 'Введите дату рождения')
-        bot.register_next_step_handler(message, get_date)
-
+        bot.send_message(message.chat.id, 'Введите отчество:')
+        bot.register_next_step_handler(message, get_surname)
 
 def callback_one(message):
-    if(message.text == 'Нет'):
-        bot.delete_message(message.chat.id, message.message_id)
-        bot.register_next_step_handler(message, start)
-        
-        
-    else:
-        bot.send_message(message.chat.id, 'OK')
-        bot.send_message(message.chat.id, 'Введите дату рождения в формате ГГ.ДД.ММ')
-        bot.register_next_step_handler(message, get_date)
-    
-        
-def callback_two(message):
     if message.text == 'Нет':
-        bot.delete_message(message.chat.id, message.message_id)
-        bot.send_message(message.chat.id, 'Введите дату рождения')
-        bot.register_next_step_handler(message,get_date)
-        
+        start(message)
     else:
-     
+        bot.send_message(message.chat.id, 'Введите дату рождения (ГГГГ.ММ.ДД):')
+        bot.register_next_step_handler(message, get_birth_date)
+
+def get_birth_date(message):
+    global birth_date
+    if message.text == 'Главное меню':
+        start(message)
+    if True:
+        birth_date = message.text
+        bot.send_message(message.chat.id, f'Дата рождения - {birth_date}. Всё верно?', reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Да'), types.KeyboardButton('Нет')))
+        bot.register_next_step_handler(message, confirm_birth_date)
+    else:
+        bot.send_message(message.chat.id, 'Введите дату рождения (ГГГГ.ММ.ДД):')
+        bot.register_next_step_handler(message, get_birth_date)
+
+def confirm_birth_date(message):
+    if message.text == 'Нет':
+        bot.send_message(message.chat.id, 'Введите дату рождения (ГГГГ.ММ.ДД):')
+        bot.register_next_step_handler(message, get_birth_date)
+    else:
+        bot.send_message(message.chat.id, 'Введите контакт пациента (например, номер телефона):')
+        bot.register_next_step_handler(message, get_contact)
+
+def get_contact(message):
+    global contact
+    if message.text == 'Главное меню':
+        start(message)
+    contact = message.text
+    bot.send_message(message.chat.id, f'Контакт пациента - {contact}. Всё верно?', reply_markup=types.ReplyKeyboardMarkup(resize_keyboard=True).add(types.KeyboardButton('Да'), types.KeyboardButton('Нет')))
+    bot.register_next_step_handler(message, confirm_contact)
+
+def confirm_contact(message):
+    if message.text == 'Нет':
+        bot.send_message(message.chat.id, 'Введите контакт пациента (например, номер телефона):')
+        bot.register_next_step_handler(message, get_contact)
+    else:
         conn = sqlite3.connect('my_database.db')
         cur = conn.cursor()
-        cur.execute("INSERT INTO data VALUES (?, ?, ?, ?, ?, ?)", (user_id, lastname, name, surname, birth_date, today1))
+        cur.execute("INSERT INTO data (tg_id, lastname, name, surname, birth_date, visit_date, contact) VALUES (?, ?, ?, ?, ?, ?, ?)",
+                    (user_id, lastname, name, surname, birth_date, today1, contact))
         conn.commit()
-        cur.close()
         conn.close()
-    
-        markup002 = types.ReplyKeyboardMarkup()
-        btn1 = types.KeyboardButton('Главное меню')
-        markup002.add(btn1)
-        bot.send_message(message.chat.id, 'OK', reply_markup=markup002)
-        
-        
-        bot.register_next_step_handler(message, start)
-        
+        bot.send_message(message.chat.id, 'Пациент добавлен!')
+        send_main_menu(message)
 
+def delete_by_id(message):
+    if message.text == 'Главное меню':
+        start(message)
+    elif message.text.isdigit():
+        id_to_delete = int(message.text)
+        conn = sqlite3.connect('my_database.db')
+        cur = conn.cursor()
+        cur.execute('DELETE FROM data WHERE id = ? AND tg_id = ?', (id_to_delete, user_id))
+        conn.commit()
+        conn.close()
+        bot.send_message(message.chat.id, f'Пациент с ID {id_to_delete} удалён.')
+        send_main_menu(message)
+    else:
+        bot.send_message(message.chat.id, 'Введите корректный числовой ID:')
+        bot.register_next_step_handler(message, delete_by_id)
 
 bot.polling(non_stop=True)
-
